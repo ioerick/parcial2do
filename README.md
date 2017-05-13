815e108c92d6faaa2ad50e501d92fd6634edd0a8
ioerick <micorreo.erick@gmail.com>
2017-05-13 08:44:08 -0400

    primer commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# segundo parcial de aco
+>ericka
---
+++ comprobar.js
@@ -0,0 +1,16 @@
+console.log("a: ");
+console.log(+ "15" + 5);
+console.log("b: ");
+console.log("88" + 1);
+console.log("c: ");
+console.log(typeof ("88" + 1));
+console.log("d: ");
+console.log(- "5" + 15);
+console.log("e: ");
+console.log(- 5 - "15");
+console.log("f: ");
+var x= 1;
+var y = 88;
+var z = 0;
+z = ++y + z;
+console.log(z);
